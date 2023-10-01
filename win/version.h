4fe6122a3ae6b46d9899eb48fed7e81bdf16cde0
blueJpg <2238288979@qq.com>
2023-10-01 10:16:29 +0800

    [+] Add icon.rc for windows

---
+++ win/version.h
@@ -0,0 +1,18 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define PRODUCT_ICON           "icon.ico" // 图标
+#define PRODUCT_NAME           "Chatbox" // 产品名称
+
+#define FILE_VERSION           1,6,0   // 文件版本
+#define FILE_VERSION_STR       "1.6.0"
+#define PRODUCT_VERSION        1,6,0   // 产品版本
+#define PRODUCT_VERSION_STR    "1.6.0"
+#define COMPANY_NAME           "Heng30"
+#define INTERNAL_NAME          "Chatbox.exe"
+#define FILE_DESCRIPTION       "Chatbox based on Slint-UI and Rust (MSVC 2022, 64 bit)"  // 文件说明
+#define LEGAL_COPYRIGHT        "Copyright 2023-2033 The Heng30 Company Ltd. All rights reserved." // 版权
+#define ORIGINAL_FILE_NAME     "Chatbox.exe"    // 原始文件名
+#define ORGANIZATION_DOMAIN    "https://heng30.xyz"  // 域名
+
+#endif // VERSION_H
